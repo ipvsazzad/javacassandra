6137feda91d66b7694564e2e34cc10fc52af5ea2
sazzad <sazzad@ipvision-inc.com>
2015-02-08 19:26:56 +0600

    method got seperated

--- src/main/java/CassandraConnector.java
+++ src/main/java/CassandraConnector.java
@@ -1,4 +1,6 @@
 import com.datastax.driver.core.Cluster;
+import com.datastax.driver.core.ResultSet;
+import com.datastax.driver.core.Row;
 import com.datastax.driver.core.Session;
 
 /**
@@ -14,13 +16,50 @@ public class CassandraConnector {
 
     public static void main(String[] args) {
         cluster = connect("localhost");
+
+
+
+
+
+        //getAllFromTable("myks", "users");
+        //createDatabase("db2");
+        //insertDataToTable("myks", "users");
+
+
+    }
+
+    private static void createDatabase(String databaseName) {
         session = cluster.connect();
-        session.execute("CREATE KEYSPACE myks WITH REPLICATION = "
+        session.execute("CREATE KEYSPACE "+databaseName+" WITH REPLICATION = "
             + "{ 'class' : 'SimpleStrategy', 'replication_factor': 1};"
         );
-        session.execute("USE mykey");
+        session.execute("USE "+databaseName);
+        session.close();
+        cluster.close();
+    }
+
+    private static void getAllFromTable(String databaseName, String tableName){
+        session = cluster.connect(databaseName);
+        ResultSet results = session.execute("SELECT * FROM "+tableName);
+        for (Row row : results) {
+            System.out.println(row.getString("lname"));
+        }
+        session.close();
+        cluster.close();
+    }
+
+    private static void insertDataToTable(String databaseName, String tableName){
+        session = cluster.connect(databaseName);
+        session.execute("INSERT INTO "+tableName+" (user_id,  fname, lname) VALUES (9945, 'sazzad', 'Islam');");
 
         session.close();
         cluster.close();
     }
 }
+
+
+
+
+
+
+
